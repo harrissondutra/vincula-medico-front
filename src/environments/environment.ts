09be7ddc2a393171b61439f1de5c019f23fb2a33
harrissondutra <harrissondutra@gmail.com>
2022-08-11 20:54:33 -0300

    Ajustes Procfile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:4042/'
+  production: true,
+  API: '/api'
 
 };
 
