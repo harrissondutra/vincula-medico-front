9a0c25b6f85fa251968bd9875aad05e67084ecf1
harrissondutra <harrissondutra@gmail.com>
2022-08-11 20:50:17 -0300

    alterações

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const jsonServer = require('json-server');
 const path = require('path');
 
 const app = express();
 
+app.use('/api', jsonServer.router('db.json'));
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/dist/vincula-medico-front'));
+app.use(express.static(__dirname +'/dist/vincula-medico-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(__dirname+'/dist/vincula-medico-front/index.html'),
+    res.sendFile(__dirname +'/dist/vincula-medico-front/index.html'),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server started on port ${port}`));
